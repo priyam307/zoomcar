13f463b6f6d44e11aed37f615341fdcc7426adec
Mac <mac@GadgetZones-MacBook-Pro.local>
2019-12-03 20:00:09 +0530

    untitled modifications

--- src/scripts/Untitled-1.js
+++ src/scripts/Untitled-1.js
@@ -1,3 +1,11 @@
 class foo {
-    
+    constructor() {
+        this.bar = 0;
+    }
+    static printValue() {
+        console.log("VAl");
+    }
+    setBar(bar) {
+        this.bar = bar;
+    }
 }
